a24053c66ca24c6a01dc64837d44339eb42c2c11
Paolo Espiritu <paoloespiritu@outlook.com>
2020-02-07 21:26:36 +0000

    helloWorld second attempt

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,7 +1,26 @@
 package main.java;
 
 class Main {
+
+    /**
+     * Author: Paolo Espiritu
+     * Created: 07/02/20
+     * 
+     * As tradition this is my first programming challenge!
+     *  - Additional task: try do this differently...
+     */
+
     public static void main(String args[]){
-        System.out.println("Hello World!");
+        System.out.println(yo());
+    }
+
+    /**
+     * 
+     * Created: 07/02/20
+     * This is my second attempt at hello world! 
+     */
+
+    public static String yo(){
+        return "Hello World!";
     }
 }
