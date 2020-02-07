f9375c41c362c1b2fb2d7d065b79f03f33185515
Paolo Espiritu <paoloespiritu@outlook.com>
2020-02-07 22:20:01 +0000

    Directory fix for solution encapsulation

--- src/main/java/challenges/1 - HelloWorld/HelloWorld.java
+++ src/main/java/challenges/1 - HelloWorld/HelloWorld.java
@@ -1,4 +1,4 @@
-package main.java.challenges;
+package main.java.challenges.HelloWorld;
 
 class HelloWorld{
 
--- src/main/java/challenges/Main.java
+++ src/main/java/challenges/Main.java
@@ -6,11 +6,8 @@ class Main {
      * Author: Paolo Espiritu
      * Created: 07/02/20
      * 
-     * As tradition this is my first programming challenge!
-     *  - Additional task: try do this differently...
      */
-
     public static void main(String args[]){
-        System.out.println(HelloWorld.helloWorld());
+        // Run your code here!
     }
 }
