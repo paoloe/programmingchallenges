0071b22699d1af9b2bb1f75d25a1e5ec520bd52d
paoloespiritu <paoloespiritu@outlook.com>
2020-02-13 11:57:54 +0000

    Cleaned up the src

---
+++ src/main/java/challenges/challenge1.java
@@ -0,0 +1,13 @@
+package main.java.challenges;
+
+class HelloWorld {
+
+    /**
+     * Challenge 1 : HelloWorld!
+     * Created 07/02/20
+     */
+
+    public static void main(String[] args){
+        System.out.print("Hello World!");
+    }
+}
